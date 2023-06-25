855663dc96b433af2b84a56e424d372a65733574
max fabin <maxikfabin@gmail.com>
2023-06-25 15:04:30 +0300

    .

--- src/components/TelegramRequire.jsx
+++ src/components/TelegramRequire.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Navigate, Outlet, useLocation} from "react-router";
+import { Outlet} from "react-router";
 import {useTelegram} from "../hooks/useTelegram.js";
 
 const TelegramRequire = () => {
     // const token = localStorage.getItem('token')
-    const location = useLocation()
+    // const location = useLocation()
     const {user} = useTelegram()
     // console.log(token)
     const goToTelegram = () => {
