ab937bdf6f5f1af478f1fa157bc5cbad53e99f84
max fabin <maxikfabin@gmail.com>
2023-06-25 15:02:30 +0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,7 @@ const Login = () => {
     const {tg,user:tgUser} = useTelegram()
     const [username, setUsername] = useState(!!tgUser?.username ? tgUser.username :'')
     const [name, setName] = useState(!!tgUser?.username ? tgUser.username :'')
+    const [user,setUser] = useState({})
     const navigate= useNavigate()
     const [findUser] = useFindUserMutation()
     const [registration] = useRegistrationMutation()
@@ -14,7 +15,7 @@ const Login = () => {
         tg.ready()
         const handleFindUser = async ()=>{
             const user = await findUser({telegramId:tgUser?.id})
-            if(!!user) navigate('/')
+            setUser(user)
         }
         if(!tgUser) {
             window.location.replace('https://t.me/spaceratebot')
@@ -43,6 +44,7 @@ const Login = () => {
             <input value={username} onChange={e=>setUsername(e.target.value)} type="text"/>
             <input value={name} onChange={e=>setName(e.target.value)} type="text"/>
             <button onClick={handleLogin}>login</button>
+            <p>{JSON.stringify(user)}</p>
         </div>
     );
 };
